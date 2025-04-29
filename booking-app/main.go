37c6b333d1c7d0987f3228682b10872fc1ac60c9
AfonsoManata <afonso.manata.cruz@gmail.com>
2025-04-29 14:57:24 +0100

    Main changes

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,62 +1,138 @@
-package main 
+package main
 
 import (
 	"fmt"
-	"strings"
+	"sync"
+	"time"
 )
 
-func main(){
-	var conferenceName = "Go conference" // Value can change
-	const conferenceTickets = 50 // Value not change 
-	var remainingTickets uint = 50 // This value will change but will be positive
+// This variables are now in the package scope "outside the Functions"
+const conferenceTickets int = 50
+var conferenceName = "Go Conference"
+var remainingTickets uint = 50
+var bookings = make([]UserData, 0)
 
-	fmt.Printf("conferenceTickets is %T \n", conferenceTickets) // %T type
+// Struc as like C 
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
 
-	fmt.Printf("Welcome to our %v booking app\n", conferenceName)		
-	fmt.Printf("We have total of %v and %v remaining Tickets!\n", conferenceTickets, remainingTickets)
-	fmt.Printf("Get your tickets here\n")
+// Go routines?
+var wg = sync.WaitGroup{}
 
-	// In go if you assign a value you dont need the data type 
+func main() {
 
-	for {
+	greetUsers()
 
-	var bookings []string // You cant mix the types	and if you dont say the size is a slice
+	// for {
+	firstName, lastName, email, userTickets := getUserInput()
+	isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+
+	if isValidName && isValidEmail && isValidTicketNumber {
+
+		bookTicket(userTickets, firstName, lastName, email)
+
+		wg.Add(1)
+		go sendTicket(userTickets, firstName, lastName, email)
+
+		firstNames := getFirstNames()
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			// end program
+			fmt.Println("Our conference is booked out. Come back next year.")
+			// break
+		}
+	} else {
+		// Simple conditions 
+		if !isValidName {
+			fmt.Println("first name or last name you entered is too short")
+		}
+		if !isValidEmail {
+			fmt.Println("email address you entered doesn't contain @ sign")
+		}
+		if !isValidTicketNumber {
+			fmt.Println("number of tickets you entered is invalid")
+		}
+	}
+	//}
+	wg.Wait()
+}
+
+func greetUsers() {
+	// Just some prints  
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+
+func getFirstNames() []string {
+	// This way of defining variables is op 
+	firstNames := []string{}
+	
+	// Use _ when is an unused variable
+	for _, booking := range bookings {
+		firstNames = append(firstNames, booking.firstName)
+	}
+	return firstNames
+}
+
+func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets uint 
+	var userTickets uint
 
-	fmt.Println("Enter you first name: ")
+	// A good upgrade to this code was giving the error after the scan 
+	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
 
-	fmt.Println("Enter you last name: ")
+	fmt.Println("Enter your last name: ")
 	fmt.Scan(&lastName)
 
-	fmt.Println("Enter you email: ")
+	fmt.Println("Enter your email address: ")
 	fmt.Scan(&email)
 
-	fmt.Println("Enter the number of tickets: ")
+	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	return firstName, lastName, email, userTickets
+}
+
+// Function to book a ticket 
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings,firstName + " " + lastName)
 
-	fmt.Printf("Number of bookings is %v\n", len(bookings))
+	// Struct defining
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
 
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings is %v\n", bookings)
 
-	// Se nao for Printf % nao funciona
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a"+ 
-		"confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v!\n", remainingTickets, conferenceName)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
+
+// Function to send a ticket
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	// time sleep in go -> use S and not s 
+	time.Sleep(5 * time.Second)
 	
-	firstNames := []string{}
+	// It formats a string and returns it 
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("#################")
+	fmt.Printf("Sending ticket:\n %v \nto email address %v\n", ticket, email)
+	fmt.Println("#################")
+	
+	wg.Done()
+}
 
-	for _, booking := range bookings { // _ -> unused variables
-		var names = strings.Fields(booking)
-		firstNames = append(firstNames, names[0])
-	}
 
-	fmt.Printf("The first names bookings are %v\n", firstNames)
-
-	}
-} 
